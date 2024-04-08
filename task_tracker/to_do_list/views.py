dd8a2b52856843d5d9c55871f988d4ed7bffe4fe
hasaniam <102344424+hasaniam@users.noreply.github.com>
2024-04-08 17:15:55 +0530

    create form

--- task_tracker/to_do_list/views.py
+++ task_tracker/to_do_list/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django .http import HttpResponse
 
+my_task =list()
 def home(request):
     #return HttpResponse('<h1>This is home page</h1>')
-    return render(request, 'home.html', {})
+
+    if request.method =="POST":
+        task = request.POST.get("task")
+        my_task.append(task)
+
+    context ={
+        "tasks": my_task,
+    }
+    return render(request, 'home.html', context)
 
 def login(request):
     #return HttpResponse('<h1>This is login page</h1>')
