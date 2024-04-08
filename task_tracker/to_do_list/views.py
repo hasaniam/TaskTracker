5962e6c025093d25f4f7d4002b5e5b374c8c619c
hasaniam <102344424+hasaniam@users.noreply.github.com>
2024-04-08 15:43:43 +0530

    create views and connect files

--- task_tracker/to_do_list/views.py
+++ task_tracker/to_do_list/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django .http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    #return HttpResponse('<h1>This is home page</h1>')
+    return render(request, 'home.html', {})
+
+def login(request):
+    #return HttpResponse('<h1>This is login page</h1>')
+    return render(request, 'login.html', {})
+def register(request):
+    #return HttpResponse('<h1>This is register page</h1>')
+    return render(request, 'register.html', {})
 
 
 
