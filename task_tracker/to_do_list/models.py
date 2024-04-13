2de9ad5fa345a8acfd0285f4535bd3faf1bd29ac
hasaniam <102344424+hasaniam@users.noreply.github.com>
2024-04-13 23:47:52 +0530

    update module

--- task_tracker/to_do_list/models.py
+++ task_tracker/to_do_list/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class Tasks(models.model):
-    Title = models.CharField(max_length=200, null= True)
-
+class Tasks(models.Model):
+    #User = models.ForeignKey()
+    Title = models.CharField(max_length=200, null=True)
+    Date = models.DateField(null=True)
+    Time = models.TimeField(null=True)
